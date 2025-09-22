3ceca3abae57c50c55c61b1ee72ac8bb37f282b1
Gustavo <gustavolimamartin2002@gmail.com>
2025-09-22 17:55:36 -0300

    Criação & Configuração
    Inicialização de projeto ASP.NET Core utilizando .NET 8.0 e criação do controller de clientes.
    
    feat(controller): adiciona controller básico de clientes, apenas modelagem;

---
+++ ProductClient/ProductClient.API/Controllers/ClientsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClient.API.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClientsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult GetByID(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public IActionResult Update()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok();
+    }
+}
